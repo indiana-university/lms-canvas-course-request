a44df00b1852b2bb473017a649a25b08fb769bac
Gregory J Thomas <gjthomas@iu.edu>
2021-09-09 08:01:14 -0400

    LMSA-7700 - removed placeholder text, added error summary w/ links, removed auto focus on inputs with errors

--- src/main/resources/static/js/siterequest.js
+++ src/main/resources/static/js/siterequest.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-    var errorFocusElements = $(".rvt-validation-danger");
-
-    if(errorFocusElements.length > 0) {
-        //always focus the first error element
-        errorFocusElements.first().focus();
-    }
-
     // this will prevent form from submitting twice
     $('#new_course_form').submit(function() {
         $('#courseSubmit').prop('disabled', true);
