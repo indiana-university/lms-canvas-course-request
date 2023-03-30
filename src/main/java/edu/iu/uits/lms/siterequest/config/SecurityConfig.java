efb05bc6a56d6915689d89df940d9a504c4ab147
Gregory J Thomas <gjthomas@iu.edu>
2023-03-30 09:59:20 -0400

    LMSA-8012 - siterequest config changes

--- src/main/java/edu/iu/uits/lms/siterequest/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/siterequest/config/SecurityConfig.java
@@ -43,6 +43,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import uk.ac.ox.ctl.lti13.Lti13Configurer;
 
 import static edu.iu.uits.lms.lti.LTIConstants.BASE_USER_ROLE;
@@ -78,6 +79,8 @@ public class SecurityConfig {
         @Autowired
         private LmsDefaultGrantedAuthoritiesMapper lmsDefaultGrantedAuthoritiesMapper;
 
+        private int yearInSeconds = 31536000;
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
@@ -86,7 +89,13 @@ public class SecurityConfig {
                     .and()
                     .authorizeRequests()
                     .antMatchers(WELL_KNOWN_ALL, "/error").permitAll()
-                    .antMatchers("/**").hasRole(BASE_USER_ROLE);
+                    .antMatchers("/**").hasRole(BASE_USER_ROLE)
+                    .and()
+                    .headers()
+                    .contentSecurityPolicy("style-src 'self'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
+                    .and()
+                    .referrerPolicy(referrer -> referrer
+                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
 
             //Setup the LTI handshake
             Lti13Configurer lti13Configurer = new Lti13Configurer()
