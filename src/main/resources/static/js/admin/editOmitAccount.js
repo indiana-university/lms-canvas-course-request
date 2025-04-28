3cef15b9221009e61eb1ae6030a813f932b41aac
Damian Sobieralski <dsobiera@iu.edu>
2025-04-28 14:50:15 -0400

    LMSA-9452 - fixing stuff after code review

--- src/main/resources/static/js/admin/editOmitAccount.js
+++ src/main/resources/static/js/admin/editOmitAccount.js
@@ -30,19 +30,17 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-$('#omitAccountTable').DataTable({
-   orderCellsTop: true,
-   paging: false,
-   order: [[0, 'asc']],
-   language: {
-       // Setting the text for the search label, mostly to remove the colon that is there by default
-       search: 'Search',
-       select: {
-          aria: {
-          }
-       }
-   },
-    columnDefs: [
-               { targets: ['.colDelete'], orderable: false }
-           ]
-   });
+jQuery(document).ready(function($) {
+    let saveButton = $("#siterequest-omitaccount-edit-save");
+
+    if (saveButton.length === 1) {
+        $("#siterequest-omitaccount-edit-save").click(function(event) {
+            let accountIdToOmitInput = $('#siterequest-omitaccount-account');
+
+            if (accountIdToOmitInput.length === 1 && accountIdToOmitInput.first().val().trim().length === 0) {
+                $("#ui-omitaccount-id-error").removeClass("rvt-display-none");
+                event.preventDefault();
+            }
+       });
+   }
+});
